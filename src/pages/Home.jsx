71b7cc690aab9bd6383a671e57d8c6ef14d286ad
Ankush <rehalankush192@gmail.com>
2025-06-26 09:48:18 +0530

    feat: added the 3d model onto the screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import {React,Suspense} from 'react'
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
+import  {Island}  from '../models/Island';
+
 const Home = () => {
+
+    const adjustIslandForScreenSize = ()=>{
+        let screenScale = null;
+        let screenPostion = [0, -6.5, -43]; 
+        let rotation = [0.1, 4.7, 0];
+
+        if(window.innerWidth < 768){
+            screenScale = [0.9,0.9,0.9];
+            screenPostion = [0, -6.5, -43];
+        }else{
+            screenScale = [1, 1, 1];
+            screenPostion = [0, -6.5, -43];
+        }
+        return [ screenScale, screenPostion, rotation]
+    }
+
+    const [islandScale, islandPostion, islandRotation] = adjustIslandForScreenSize();
     return (
         <section className='w-full h-screen relative'>
             {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -17,6 +36,10 @@ const Home = () => {
                 <pointLight/>
                 <spotLight/>
                 <hemisphereLight/>
+                <Island 
+                position = {islandPostion}
+                scale = {islandScale}
+                rotation = {islandRotation}/>
                 </Suspense>
             </Canvas>
         </section>
